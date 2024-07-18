70d2d49ebe5431fc7c9c506053ef6102e74f82da
Alessio Vietri <info@alessiovietri.it>
2024-07-18 12:32:05 +0200

    Completato LC

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,38 @@
-console.log('JS collegato');
+const userPosition = parseInt(prompt('In che posizione sei arrivato?'));
+
+const primoPosto = 1;
+const secondoPosto = 2;
+const terzoPosto = 3;
+
+// if (userPosition === primoPosto) {
+//     alert('Wow! Sei arrivato primo! Complimenti!');
+// }
+// else if (userPosition === secondoPosto) {
+//     alert('Bravo, sei arrivato secondo!');
+// }
+// else if (userPosition === terzoPosto) {
+//     alert('Ottimo, sei sul podio!');
+// }
+// else {
+//     alert('Sei arrivato in posizione ' + userPosition);
+// }
+
+switch (userPosition) {
+
+    case primoPosto:        // if (userPosition === primoPosto)
+        alert('Wow! Sei arrivato primo! Complimenti!');
+        break;
+
+    case secondoPosto:      // else if (userPosition === secondoPosto)
+        alert('Bravo, sei arrivato secondo!');
+        break;
+
+    case terzoPosto:        // else if (userPosition === terzoPosto)
+        alert('Ottimo, sei sul podio!');
+        break;
+
+    default:                // else
+        alert('Sei arrivato in posizione ' + userPosition);
+        break;
+
+}
